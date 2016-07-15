e6b09f25a09493f624d6e591e5f954a122fd2c99
Baiju Achari <baijuachari@gmail.com>
2016-07-14 21:15:26 -0400

    Added basic classes

---
+++ SpringSamples/SpringCore/src/com/baiju/spcore/Address.java
@@ -0,0 +1,20 @@
+package com.baiju.spcore;
+
+public class Address {
+	
+	private String location;
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String toString() {
+		return("Location:  " + this.location);
+		
+	}
+	
+}
---
+++ SpringSamples/SpringCore/src/com/baiju/spcore/Employee.java
@@ -0,0 +1,17 @@
+package com.baiju.spcore;
+
+public class Employee {
+	
+	private String name;
+	
+	private Address address;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}	
+
+}
